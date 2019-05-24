c22d72d9acd7f5ca7a72b3f899d4f79fc8736700
SeungsooKang <seungsoo@sheridancollege.ca>
2019-05-23 23:27:19 -0400

    create Player Class

---
+++ DiceeGame/Player.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceeGame
+{
+    class Player
+    {
+        public string Name { get; set; }
+        public int Score { get; set; }
+        public List<RollResult> History { get; set; }
+
+        public Player()
+        {
+            History = new List<RollResult>();
+        }
+    }
+}
