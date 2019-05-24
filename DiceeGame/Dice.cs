7bd3a5c1bb672aed233468610e60e8c67f942be3
SeungsooKang <seungsoo@sheridancollege.ca>
2019-05-23 23:20:11 -0400

    create RollResult Enum

---
+++ DiceeGame/Dice.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceeGame
+{
+    class Dice
+    {
+        private int _face;
+        public int Face
+        {
+            get { return _face; }
+        }
+
+        private int _max;
+        public int Max
+        {
+            get { return _max; }
+            set { _max = value; }
+        }
+
+        public Dice(int max = 6)
+        {
+            _max = max;
+        }
+
+        public void Roll()
+        {
+            Random rnd = new Random();
+            _face = rnd.Next(1, _max + 1);
+        }
+
+    }
+}
---
+++ DiceeGame/DiceGame.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceeGame
+{
+    enum RollResult
+    {
+        Jackpot,    // all the dice have the max number
+        Win,        // all the dice have the same number, but not the max
+        Lose        // different numbers
+    }
+
+    class DiceGame
+    {
+    }
+}
